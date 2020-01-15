8152701486edd546f8dfec68e0ec2f99860d9c85
qsaleh <qmsaleh@gmail.com>
2020-01-15 23:47:25 +0000

    created input.js file

---
+++ input.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput); 
+
+  function handleUserInput(key) {
+  if (key === '\u0003'){
+  process.exit();
+  }
+  return stdin;
+}
+
+}
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input')
+
 console.log('Connecting ...');
 connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput); 
-
-  function handleUserInput(key) {
-  if (key === '\u0003'){
-  process.exit();
-  }
-  return stdin;
-}
-
-}
 
 setupInput();
