b5cf0279f80125847a3e44b911c92874b93cdde9
qsaleh <qmsaleh@gmail.com>
2020-01-16 00:37:25 +0000

    update to allow snake to move

--- input.js
+++ input.js
@@ -1,7 +1,10 @@
 const net = require('net');
+const stdin = process.stdin;
+let connection;
 
-const setupInput = function() {
-  const stdin = process.stdin;
+const setupInput = function(conn) {
+  connection = conn;
+
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
@@ -10,8 +13,16 @@ const setupInput = function() {
   function handleUserInput(key) {
   if (key === '\u0003'){
   process.exit();
+  } else if (key === 'w'){
+    conn.write('Move: up');
+  } else if (key === 'a'){
+    conn.write('Move: left');
+  } else if (key === 's'){
+    conn.write('Move: down');
+  } else if (key === 'd'){
+    conn.write('Move: right');
   }
-  return stdin;
+ return stdin; 
 }
 
 }
